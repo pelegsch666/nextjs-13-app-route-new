3da95ef705b9853ec1f65f6bce3f4366284064ae
pelegsch666 <peleg.mazuz@gmail.com>
2023-12-17 18:49:30 +0200

    check

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -3,31 +3,38 @@ import Navbar from '@/shared/Navbar';
 import { getHtmlDirection } from '@/utils/getHtmlDirection';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import StyledComponentsRegistry from '../../lib/registry';
+import GlobalStyles from '../../styles/GlobalStyles';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Next.js 13 & i18n Template',
-	description: 'Template for Next.js 13 with App Router and i18n',
+  title: 'Next.js 13 & i18n Template',
+  description: 'Template for Next.js 13 with App Router and i18n',
 };
 
 export async function generateStaticParams() {
-	return i18n.locales.map((locale) => ({ lang: locale }));
+  return i18n.locales.map(locale => ({ lang: locale }));
 }
 
 export default function RootLayout({
-	children,
-	params,
+  children,
+  params,
 }: {
-	children: React.ReactNode;
-	params: { lang: Locale };
+  children: React.ReactNode;
+  params: { lang: Locale };
 }) {
-	return (
-		<html lang={params.lang} dir={getHtmlDirection(params.lang)}>
-			<body className={inter.className}>
-				<Navbar params={params} />
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html
+      lang={params.lang}
+      dir={getHtmlDirection(params.lang)}>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+		<GlobalStyles />
+          <Navbar params={params} />
+          {children}
+        </StyledComponentsRegistry>
+      </body>
+    </html>
+  );
 }
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,11 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+
+const styles = css`
+  // your global styles
+`;
+const GlobalStyles = createGlobalStyle`
+   ${styles}
+`;
+
+export default GlobalStyles;
