6b7c7cbb66f09ecf17fa37c2ab8fd10341486e83
pelegsch666 <peleg.mazuz@gmail.com>
2023-12-19 17:54:29 +0200

    fixed

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,6 +5,11 @@ const theme = {
     colorName3: 'hsla(294, 69%, 34%, 0.5)',
     colorName4: 'hsla(100, 69%, 34%, 0.5)',
     colorName5: 'hsla(222, 84%, 50%, 0.654)',
+    colorName6: 'hsla(222, 84%, 50%, 0.654)',
+    colorName7: 'hsla(222, 84%, 50%, 0.654)',
+    colorName8: 'hsla(222, 84%, 50%, 0.654)',
+    colorName9: 'hsla(222, 84%, 50%, 0.654)',
+    
   },
 };
 
